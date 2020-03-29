d73dd1c18dd583862763da136866e714bcb0de71
kanishq1 <kanishqkhandelwal@gmail.com>
2020-03-29 15:50:59 +0530

    added

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let info=[];
+let infected=[];
+let i=1;
 function onFormSubmit() {
     let data = {
+        id:i,
         name:document.getElementById('name').value,
         age:document.getElementById('age').value,
         location:document.getElementById('location').value,
       }
-      info.push(data);
-      console.log('added', {info});
+      infected.push(data);
+      console.log('added', {infected});
+      i++;
    
 }
